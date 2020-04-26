ddb0d26c097fd9135cc2d5f185e485e69e3b01d8
pinakipatrapro <pinaki.patra@gds.ey.com>
2020-04-26 11:53:16 +0530

    Image Build

--- Image Downloader.py
+++ Image Downloader.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 import urllib.request
 import uuid
 import os, re, os.path
@@ -15,27 +9,7 @@ for root, dirs, files in os.walk(mypath):
 
 for x in range(2):
     print(x)
-#     urllib.request.urlretrieve("https://picsum.photos/4000/2000", "loreum/{}.jpg".format(uuid.uuid1()))
     urllib.request.urlretrieve("https://unsplash.it/1920/1080?random", "DailyUpdated/{}.jpg".format(uuid.uuid1()))
-    
-    
-
-
-# In[ ]:
-
-
-import uuid
-uuid.uuid1()
-
-
-# In[11]:
-
-
-for x in range(6):
-  print(x)
-
-
-# In[ ]:
 
 
 
